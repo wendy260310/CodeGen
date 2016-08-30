3bb9ccf111d5454a1b6777f119e088a551168863
zhongchaojun <zhongchaojun@xiaomi.com>
2016-08-30 17:53:25 +0800

    init commit
    N/A
    
    Signed-off-by: zhongchaojun <zhongchaojun@xiaomi.com>

---
+++ code_gen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# check input
+if [ "$#" -ne 2 ]
+then
+	echo "two parameters , first is sql ,second is dao directory";
+	exit 1
+fi
+# check input illegal
+if [ -r "$1" ]
+then
+	echo "$1 can be read , file check success "
+else
+	echo "have no permission to read file $1"
+	exit 1
+fi
+if [ -w "$2" ]
+then
+	echo " $2 can be access , directory check success "
+else
+	echo " have no permission to write file to $2 "
+	exit 1
+fi
+
+table_create="create table";
+
+# read sql
+
+while IFS='' read -r line || [[ -n "$line" ]] ; do
+	content=$( echo "$line" | awk ' {print tolower($0)}' )
+done < "$1"
